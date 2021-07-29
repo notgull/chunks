c2a75ab04126272e0416cb607776725c297e6548
not_a_seagull <notaseagull048@gmail.com>
2021-07-29 15:03:18 -0700

    Improved again

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 #![no_std]
 #![feature(control_flow_enum)]
 #![feature(maybe_uninit_array_assume_init)]
+#![feature(maybe_uninit_extra)]
 #![feature(maybe_uninit_slice)]
 #![feature(maybe_uninit_uninit_array)]
 #![feature(iter_advance_by)]
@@ -11,8 +12,8 @@
 
 use core::{
     iter::{FusedIterator, TrustedLen},
-    mem::MaybeUninit,
-    ops::{ControlFlow, Try},
+    mem::{ManuallyDrop, MaybeUninit},
+    ops::{ControlFlow, Range, Try},
     ptr,
 };
 
@@ -111,7 +112,7 @@ impl<I: Iterator, const N: usize> Chunks<I, N> {
     ///
     /// ```
     /// use chunks::IteratorExt;
-    /// 
+    ///
     /// let data = vec![1, 2, 3, 4, 5, 6, 7, 8];
     /// let mut chunks = data.into_iter().chunks::<3>();
     /// assert_eq!(chunks.next(), Some([1, 2, 3]));
@@ -121,6 +122,51 @@ impl<I: Iterator, const N: usize> Chunks<I, N> {
     /// ```
     pub fn remainder_mut(&mut self) -> &mut [I::Item] {
         self.buffer.initialized_slice_mut()
+    }
+
+    /// Gets an iterator over the remaining items left over by the chunking process. This iterator will be empty
+    /// if the `Chunks` has not completed or if every item of the `Chunks` fit into the resulting arrays.
+    ///
+    /// # Example
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// use chunks::IteratorExt;
+    ///
+    /// let data = vec![1, 2, 3, 4, 5, 6, 7, 8];
+    /// let mut chunks = data.into_iter().chunks::<3>();
+    /// 
+    /// // discard every element in the iterator
+    /// while let Some(_) = chunks.next() {}
+    /// 
+    /// let mut remainder = chunks.into_remainder();
+    ///
+    /// assert_eq!(remainder.next(), Some(7));
+    /// assert_eq!(remainder.next(), Some(8));
+    /// assert_eq!(remainder.next(), None);
+    /// ```
+    ///
+    /// [`IntoRemainder`] implements `DoubleEndedIterator`, so it can also be iterated from the back, if need be.
+    ///
+    /// ```
+    /// use chunks::IteratorExt;
+    ///  
+    /// let data = vec![1, 2, 3, 4, 5, 6, 7, 8];
+    /// let mut chunks = data.into_iter().chunks::<3>();
+    /// while let Some(_) = chunks.next() {}
+    /// let mut remainder = chunks.into_remainder();
+    ///
+    /// assert_eq!(remainder.next_back(), Some(8));
+    /// assert_eq!(remainder.next_back(), Some(7));
+    /// assert_eq!(remainder.next_back(), None);
+    /// ```  
+    pub fn into_remainder(self) -> IntoRemainder<I::Item, N> {
+        let (buffer, initialized) = self.buffer.into_raw_parts();
+        IntoRemainder {
+            remainder: buffer,
+            initialized: 0..initialized,
+        }
     }
 }
 
@@ -204,6 +250,80 @@ impl<I: DoubleEndedIterator, const N: usize> DoubleEndedIterator for Chunks<I, N
 
 // SAFETY: our size is exact, see above
 unsafe impl<I: TrustedLen, const N: usize> TrustedLen for Chunks<I, N> {}
+
+/// An iterator over the remainder that a [`Chunks`] leaves behind.
+///
+/// This iterator is returned by the [`into_remainder`] method on [`Chunks`]. See its documentation for more
+/// information.
+#[derive(Debug)]
+pub struct IntoRemainder<I, const N: usize> {
+    /// Internal buffer containing remainder items.
+    remainder: [MaybeUninit<I>; N],
+    /// Range over which `remainder` is valid.
+    initialized: Range<usize>,
+}
+
+impl<I: Clone, const N: usize> Clone for IntoRemainder<I, N> {
+    fn clone(&self) -> IntoRemainder<I, N> {
+        let mut into_remainder = IntoRemainder {
+            remainder: MaybeUninit::uninit_array(),
+            initialized: self.initialized.start..self.initialized.start,
+        };
+
+        for i in self.initialized.clone() {
+            // SAFETY: remainder[i] is guaranteed to be initialized
+            into_remainder.remainder[i] = MaybeUninit::new(unsafe {
+                MaybeUninit::assume_init_read(&self.remainder[i]).clone()
+            });
+
+            // increment initialized range by 1
+            into_remainder.initialized.end += 1;
+        }
+
+        into_remainder
+    }
+}
+
+impl<I, const N: usize> Iterator for IntoRemainder<I, N> {
+    type Item = I;
+
+    fn next(&mut self) -> Option<I> {
+        // SAFETY: remainder at "i" is guaranteed to be valid
+        self.initialized
+            .next()
+            .map(|i| unsafe { MaybeUninit::assume_init_read(&self.remainder[i]) })
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.initialized.size_hint()
+    }
+}
+
+impl<I, const N: usize> FusedIterator for IntoRemainder<I, N> {}
+impl<I, const N: usize> ExactSizeIterator for IntoRemainder<I, N> {}
+
+impl<I, const N: usize> DoubleEndedIterator for IntoRemainder<I, N> {
+    fn next_back(&mut self) -> Option<I> {
+        // SAFETY: same as above
+        self.initialized
+            .next_back()
+            .map(|i| unsafe { MaybeUninit::assume_init_read(&self.remainder[i]) })
+    }
+}
+
+impl<I, const N: usize> Drop for IntoRemainder<I, N> {
+    fn drop(&mut self) {
+        let valid_ptr = MaybeUninit::slice_as_mut_ptr(&mut self.remainder);
+        // SAFETY: valid_ptr is an array, guaranteed to be valid for "initialized"
+        let valid_ptr = unsafe { valid_ptr.add(self.initialized.start) };
+        let valid = ptr::slice_from_raw_parts_mut(valid_ptr, self.initialized.len());
+
+        // SAFETY: because of initialized, this is guaranteed to be valid
+        unsafe {
+            ptr::drop_in_place(valid);
+        }
+    }
+}
 
 /// Helper method for next(); fills up the chunk buffer and breaks when it is full.
 #[inline]
@@ -369,6 +489,14 @@ impl<Item, const N: usize> ChunkBuffer<Item, N> {
         // SAFETY: same as above
         unsafe { MaybeUninit::slice_assume_init_mut(&mut self.buffer[..self.initialized]) }
     }
+
+    /// Converts this `ChunkBuffer` into its raw parts without dropping the memory involved.
+    fn into_raw_parts(self) -> ([MaybeUninit<Item>; N], usize) {
+        let this = ManuallyDrop::new(self);
+        // SAFETY: since we are not dropped "this", "buffer" is never accessed again
+        let buffer = unsafe { ptr::read(&this.buffer) };
+        (buffer, this.initialized)
+    }
 }
 
 impl<Item, const N: usize> Drop for ChunkBuffer<Item, N> {
@@ -415,5 +543,6 @@ mod tests {
     fn custom_advance_by() {
         let elems = &[1, 2, 3, 4, 5, 6, 7];
         assert_eq!(elems.iter().copied().chunks::<2>().nth(2), Some([5, 6]));
+        assert_eq!(elems.iter().copied().chunks::<2>().nth(3), None);
     }
 }
